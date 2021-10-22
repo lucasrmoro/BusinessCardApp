c7a79fe01d108f46bba8da656919842a7c3577a6
lucasrmoro <lucas.rmoro1@gmail.com>
2021-10-22 15:21:16 -0300

    Implementing ViewBinding on project and inserting some listeners.

--- app/src/main/java/br/com/lucas/businesscardapp/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/lucas/businesscardapp/ui/AddBusinessCardActivity.kt
@@ -1,11 +1,35 @@
 package br.com.lucas.businesscardapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.lucas.businesscardapp.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_business_card)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.btnClose.setOnClickListener {
+            finish()
+        }
+        binding.btnSave.setOnClickListener {
+
+        }
+    }
+
+    companion object {
+        fun launchScreen(context: Context) {
+            val intent = Intent(context, AddBusinessCardActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 }
--- app/src/main/java/br/com/lucas/businesscardapp/ui/MainActivity.kt
+++ app/src/main/java/br/com/lucas/businesscardapp/ui/MainActivity.kt
@@ -1,12 +1,23 @@
 package br.com.lucas.businesscardapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.lucas.businesscardapp.R
+import androidx.appcompat.app.AppCompatActivity
+import br.com.lucas.businesscardapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.addFab.setOnClickListener {
+            AddBusinessCardActivity.launchScreen(this)
+        }
     }
 }
